e21097d04fb3484b923b47ff2f898c5911ab3d27
Filipa Flora <filipa.flora@gmail.com>
2023-09-06 00:54:02 +0100

    Added styling for Login Page

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,13 +1,21 @@
-import { useContext, useState } from 'react';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../Context/auth.context';
+import { useContext, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Context/auth.context";
 
-const API_URL = 'http://localhost:5005';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+const API_URL = "http://localhost:5005";
 
 function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
 
   const { storeToken, authenticateUser } = useContext(AuthContext);
@@ -24,7 +32,7 @@ function LoginPage() {
       .then((response) => {
         storeToken(response.data.authToken);
         authenticateUser();
-        navigate('/');
+        navigate("/");
       })
       .catch((error) => {
         const errorDescription = error.response.data.message;
@@ -33,33 +41,111 @@ function LoginPage() {
   };
 
   return (
-    <div className="loginPage">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <button type="submit">Login</button>
-      </form>
-      {errorMessage && <p>{errorMessage}</p>}
-      <p>Do not have an account yet?</p>
-      <Link to="/signup">Sign Up</Link>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage: "url(/images/LoginPage.jpg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Container
+        component="main"
+        maxWidth="xs"
+      >
+        <div style={{ background: "rgba(51, 51, 51, 0.90)", color: "white" }}>
+          <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <Avatar sx={{p: '10px', m: '30px 0 10px', bgcolor: "#227A60" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+
+            <Typography component="h1" variant="h5">
+              Log in
+            </Typography>
+
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{
+                mt: 1,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: '30px',
+              }}
+            >
+              <TextField
+                margin="normal"
+                required
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={(e) => setEmail(e.target.value)}
+                sx={{ width: "100%", color: "white" }}
+              />
+              <TextField
+                margin="normal"
+                required
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+                sx={{ width: "100%", color: "white" }}
+              />
+
+              <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2, backgroundColor: '#227A60' }}>
+                Log In
+              </Button>
+              {errorMessage && <p>{errorMessage}</p>}
+              <Typography variant="body1">
+                {"Don't have an account?"} <Link to={'/signup'} style={{color: '#227A60'}}> Sign Up</Link>
+              </Typography>
+              
+            </Box>
+          </Box>
+        </div>
+      </Container>
     </div>
   );
 }
 export default LoginPage;
+
+// return (
+//   <div className="loginPage">
+//     <h1>Login</h1>
+//     <form onSubmit={handleSubmit}>
+//       <label>
+//         Email:
+//         <input
+//           type="email"
+//           name="email"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//       </label>
+//       <label>
+//         Password:
+//         <input
+//           type="password"
+//           name="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//       </label>
+//       <button type="submit">Login</button>
+//     </form>
+//     {errorMessage && <p>{errorMessage}</p>}
+//     <p>Do not have an account yet?</p>
+//     <Link to="/signup">Sign Up</Link>
+//   </div>
+// );
