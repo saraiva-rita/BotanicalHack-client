23160df95f513d15fb3348e0dafe822450c8066e
Rita Saraiva <rita_saraiva@hotmail.com>
2023-09-05 22:17:03 +0100

    plant details

--- src/Pages/PlantsDetails/index.jsx
+++ src/Pages/PlantsDetails/index.jsx
@@ -1,23 +1,27 @@
-import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-import axios from "axios";
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import * as React from 'react';
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab from '@mui/joy/Tab';
+import TabPanel from '@mui/joy/TabPanel';
 
 function PlantDetails() {
   const [foundPlant, setfoundPlant] = useState(null);
 
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState('');
   const [rating, setRating] = useState(1);
 
   const [shouldGetPlant, setShouldGetPlant] = useState(true);
 
   const [user, setUser] = useState(null);
 
-  const API_URL = "http://localhost:5005";
+  const API_URL = 'http://localhost:5005';
 
   const { plantId } = useParams();
 
-  const storedToken = localStorage.getItem("authToken");
-  
+  const storedToken = localStorage.getItem('authToken');
 
   useEffect(() => {
     if (shouldGetPlant) {
@@ -79,7 +83,7 @@ function PlantDetails() {
   // Add Plants to MyPlants
   const addMyPlants = () => {
     axios
-      .post(`${API_URL}/api/plants/${plantId}/addMyPlants`, "", {
+      .post(`${API_URL}/api/plants/${plantId}/addMyPlants`, '', {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then(() => {
@@ -91,7 +95,7 @@ function PlantDetails() {
   // Add Plants to Wish List
   const addWishList = () => {
     axios
-      .post(`${API_URL}/api/plants/${plantId}/addToWishList`, "", {
+      .post(`${API_URL}/api/plants/${plantId}/addToWishList`, '', {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then(() => {
@@ -105,57 +109,84 @@ function PlantDetails() {
       <div className="plants-details-banner"></div>
       <div>
         {foundPlant && (
-          <div>
-            <h1>{foundPlant.name}</h1>
-            <button onClick={addMyPlants}>Add to My Plants</button>
-            <button onClick={addWishList}>Add to My Wish List</button>
-            <img src={foundPlant.image} alt="Plant image" />
-            <p>{foundPlant.description}</p>
-            <p>{foundPlant.tips}</p>
-
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
             <div>
-              {foundPlant.reviews.map((review) => {
-                return (
-                  <div key={review._id}>
-                    <p>{review.content}</p>
-                    <p>{review.rating}</p>
-                    <p>{review.author.name}</p>
-                    {user.reviews.includes(review._id) && (
-                      <button onClick={() => deleteReview(review._id)}>
-                        Delete
-                      </button>
-                    )}
-                  </div>
-                );
-              })}
+              <img
+                className="details-img"
+                src={foundPlant.image}
+                alt="Plant image"
+              />
             </div>
+            <div className="details-content">
+              <h1>{foundPlant.name}</h1>
+              <div>
+                <button onClick={addMyPlants}>Add to My Plants</button>
+                <button onClick={addWishList}>Add to My Wish List</button>
+              </div>
+              <Tabs aria-label="Basic tabs" defaultValue={0}>
+                <TabList>
+                  <Tab>Description</Tab>
+                  <Tab>Care Tips</Tab>
+                  <Tab>Reviews</Tab>
+                </TabList>
+                <TabPanel value={0}>
+                  <p>{foundPlant.description}</p>
+                </TabPanel>
+                <TabPanel value={1}>
+                  <p>{foundPlant.tips}</p>
+                </TabPanel>
+                <TabPanel value={2}>
+                  <div>
+                    {foundPlant.reviews.map((review) => {
+                      return (
+                        <div key={review._id}>
+                          <p>{review.content}</p>
+                          <p>{review.rating}</p>
+                          <p>{review.author.name}</p>
+                          {user.reviews.includes(review._id) && (
+                            <button onClick={() => deleteReview(review._id)}>
+                              Delete
+                            </button>
+                          )}
+                        </div>
+                      );
+                    })}
+                  </div>
 
-            <form onSubmit={handleSubmitAddReview}>
-              <label>
-                Content:
-                <input
-                  type="text"
-                  name="content"
-                  value={content}
-                  onChange={(e) => setContent(e.target.value)}
-                />
-              </label>
+                  <form onSubmit={handleSubmitAddReview}>
+                    <label>
+                      Content:
+                      <input
+                        type="text"
+                        name="content"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                      />
+                    </label>
 
-              <label>
-                Rating:
-                <input
-                  type="number"
-                  name="rating"
-                  value={rating}
-                  onChange={(e) => setRating(e.target.value)}
-                />
-              </label>
+                    <label>
+                      Rating:
+                      <input
+                        type="number"
+                        name="rating"
+                        value={rating}
+                        onChange={(e) => setRating(e.target.value)}
+                      />
+                    </label>
 
-              <button type="submit">Create Review</button>
-            </form>
+                    <button type="submit">Create Review</button>
+                  </form>
+                </TabPanel>
+              </Tabs>
+            </div>
           </div>
         )}
-        <Link to={"/plants"}>Back</Link>
+        <Link to={'/plants'}>Back</Link>
       </div>
     </div>
   );
