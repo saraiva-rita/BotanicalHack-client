1ea72ff2a97c3033cfa4fb844f2030881761b8b9
Filipa Flora <filipa.flora@gmail.com>
2023-09-07 00:07:25 +0100

    created theme to change colors of MUI components

--- src/App.jsx
+++ src/App.jsx
@@ -11,57 +11,77 @@ import PlantDetails from "./Pages/PlantsDetails";
 import ProfilePage from "./Pages/ProfilePage";
 import MyPlantsPage from "./Pages/MyPlants";
 import WishListPage from "./Pages/WishList";
+import About from "./Pages/AboutPage";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    green: {
+      main: "#227A60",
+      contrastText:"#ffffff"
+    },
+    red: {
+      main: "#c62828",
+    },
+    orange: {
+      main: 'rgba(220, 117, 42, 0.7)',
+    }
+  },
+});
 function App() {
   return (
     <div>
-      <Navbar />
+      <ThemeProvider theme={theme}>
+        <Navbar />
 
-      <Routes>
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/plants" element={<AllPlantsPage />} />
-        <Route
-          path="/plants/:plantId"
-          element={
-            <IsPrivate>
-              <PlantDetails />
-            </IsPrivate>
-          }
-        />
-        <Route
-          path="/myPlants"
-          element={
-            <IsPrivate>
-              <MyPlantsPage />
-            </IsPrivate>
-          }
-        />
-        <Route
-          path="/wishList"
-          element={
-            <IsPrivate>
-              <WishListPage />
-            </IsPrivate>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <IsAnon>
-              <SignupPage />
-            </IsAnon>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <IsAnon>
-              <LoginPage />
-            </IsAnon>
-          }
-        />
-      </Routes>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/plants" element={<AllPlantsPage />} />
+          <Route
+            path="/plants/:plantId"
+            element={
+              <IsPrivate>
+                <PlantDetails />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/myPlants"
+            element={
+              <IsPrivate>
+                <MyPlantsPage />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/wishList"
+            element={
+              <IsPrivate>
+                <WishListPage />
+              </IsPrivate>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <IsAnon>
+                <SignupPage />
+              </IsAnon>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <IsAnon>
+                <LoginPage />
+              </IsAnon>
+            }
+          />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
