8d9266a675a40b732e769808680db80041249e4a
Filipa Flora <filipa.flora@gmail.com>
2023-09-06 18:40:31 +0100

    fixed dynamic banner in allPlants

--- src/Pages/AllPlants/index.jsx
+++ src/Pages/AllPlants/index.jsx
@@ -1,28 +1,28 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import SearchBar from '../../Components/Searchbar';
-import { useLocation } from 'react-router-dom';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import SearchBar from "../../Components/Searchbar";
+import { useLocation } from "react-router-dom";
 
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 // const API_URL = 'https://botanicalhack.onrender.com';
-const API_URL = 'http://localhost:5005';
+const API_URL = "http://localhost:5005";
 
 function AllPlantsPage() {
   const [plants, setPlants] = useState([]);
   const [filteredPlant, setFilteredPlant] = useState(plants);
   const location = useLocation();
-  const { category, description } = location.state || {};
+  const { category, description, image } = location.state || {};
 
   useEffect(() => {
     axios
       .get(`${API_URL}/api/plants`)
       .then((response) => {
         if (category) {
-          console.log('category', category);
+          console.log("category", category);
           const result = response.data.filter(
             (plant) => plant.category === category
           );
@@ -37,7 +37,7 @@ function AllPlantsPage() {
   }, [category]);
 
   const searchPlant = (searchedName) => {
-    if (searchedName === '') {
+    if (searchedName === "") {
       setFilteredPlant(plants);
       return;
     }
@@ -50,32 +50,27 @@ function AllPlantsPage() {
 
   return (
     <div>
-      {category && (
-        <div className="hero-banner-categories">
-          <h3>{category}</h3>
-          <p>{description}</p>
-        </div>
-      )}
-      <div className="all-plants-page">
+      <div
+        style={{ height: '450px', position: 'relative', backgroundImage: image ? `url(${image})` : "url(/images/AllPlantsPage.jpg)", backgroundRepeat: 'no-repeat'}}
+      >
         <div className="details-banner-wrapper yellow-color">
           <div className="banner-content">
-            <h2>All Plants</h2>
+            <h3>{category ?? "All Plants"}</h3>
             <p>
-              Explore a Diverse Array of Green Beauties and Find the Perfect
-              Additions for Your Oasis🍀Scroll Through a Verdant Wonderland Your
-              Garden Awaits!
+              {description ??
+                "Explore a Diverse Array of Green Beauties and Find the Perfect Additions for Your Oasis 🍀 Scroll Through a Verdant Wonderland Your Garden Awaits!"}
             </p>
           </div>
         </div>
       </div>
-
+  
       <SearchBar searchPlant={searchPlant} />
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          minHeight: '100vh',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
         }}
       >
         <Box sx={{ width: 1350, height: 730 }}>
@@ -85,21 +80,21 @@ function AllPlantsPage() {
                 return (
                   <ImageListItem
                     key={plant._id}
-                    sx={{ border: 1, borderColor: 'grey.500' }}
+                    sx={{ border: 1, borderColor: "grey.500" }}
                   >
                     <div key={plant._id}>
                       <Link
                         to={`/plants/${plant._id}`}
-                        style={{ textDecoration: 'none', color: 'inherit' }}
+                        style={{ textDecoration: "none", color: "inherit" }}
                       >
                         <img
                           src={`${plant.image}?w=50&fit=crop&auto=format`}
                           srcSet={`${plant.image}?w=50&fit=crop&auto=format&dpr=2 2x`}
                           alt={plant.name}
                           loading="lazy"
-                          style={{ width: '100%', height: 'auto' }}
+                          style={{ width: "100%", height: "auto" }}
                         />
-                        <h3 style={{ textAlign: 'center' }}>{plant.name} </h3>
+                        <h3 style={{ textAlign: "center" }}>{plant.name} </h3>
                       </Link>
                     </div>
                   </ImageListItem>
