b2a77a816e8416b61bda00f0ee76fc9fcee5090c
Filipa Flora <filipa.flora@gmail.com>
2023-10-18 15:37:58 +0100

    fixed default theme of MUI

--- src/App.jsx
+++ src/App.jsx
@@ -21,82 +21,75 @@ import {
 } from "@mui/material/styles";
 import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
 
-
-//const materialTheme = materialExtendTheme();
-
-
-const materialTheme = materialExtendTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#227A60",
-      // contrastText: "#ffffff",
-    },
-    secondary: {
-      main: "#c62828",
-    },
-  },
-});
-
 function App() {
+  const materialTheme = materialExtendTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#227A60",
+      },
+      secondary: {
+        main: "#c62828",
+      },
+    },
+  });
+
   return (
     <div>
       <MaterialCssVarsProvider
         defaultMode="light"
         theme={{ [MATERIAL_THEME_ID]: materialTheme }}
       >
-        <JoyCssVarsProvider defaultMode="light">
-          {/* <ThemeProvider theme={theme}> */}
-            <Navbar />
+        <JoyCssVarsProvider>
+          <Navbar />
 
-            <Routes>
-              <Route path="/about" element={<About />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/" element={<HomePage />} />
-              <Route path="/plants" element={<AllPlantsPage />} />
-              <Route
-                path="/plants/:plantId"
-                element={
-                  <IsPrivate>
-                    <PlantDetails />
-                  </IsPrivate>
-                }
-              />
-              <Route
-                path="/myPlants"
-                element={
-                  <IsPrivate>
-                    <MyPlantsPage />
-                  </IsPrivate>
-                }
-              />
-              <Route
-                path="/wishList"
-                element={
-                  <IsPrivate>
-                    <WishListPage />
-                  </IsPrivate>
-                }
-              />
-              <Route
-                path="/signup"
-                element={
-                  <IsAnon>
-                    <SignupPage />
-                  </IsAnon>
-                }
-              />
-              <Route
-                path="/login"
-                element={
-                  <IsAnon>
-                    <LoginPage />
-                  </IsAnon>
-                }
-              />
-            </Routes>
-          {/* </ThemeProvider> */}
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/plants" element={<AllPlantsPage />} />
+            <Route
+              path="/plants/:plantId"
+              element={
+                <IsPrivate>
+                  <PlantDetails />
+                </IsPrivate>
+              }
+            />
+            <Route
+              path="/myPlants"
+              element={
+                <IsPrivate>
+                  <MyPlantsPage />
+                </IsPrivate>
+              }
+            />
+            <Route
+              path="/wishList"
+              element={
+                <IsPrivate>
+                  <WishListPage />
+                </IsPrivate>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <IsAnon>
+                  <SignupPage />
+                </IsAnon>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <IsAnon>
+                  <LoginPage />
+                </IsAnon>
+              }
+            />
+          </Routes>
         </JoyCssVarsProvider>
       </MaterialCssVarsProvider>
     </div>
